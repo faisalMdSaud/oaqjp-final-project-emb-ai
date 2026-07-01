f8d083976c891f0f8bcfb6b6814dee575041ac26
faisalMdSaud <saud-al-faisal@outlook.com>
2026-07-01 11:35:21 -0400

    made package EmotionDetection

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,9 +6,30 @@ def emotion_detector(text_to_analyze):
     inputObj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = inputObj, headers = headers)
     
+
     if response.status_code == 200:
         formatted_response = json.loads(response.text)
-        print(formatted_response)
+        # 1. Unpack the list to isolate the target emotions dictionary
+        [prediction_details] = formatted_response['emotionPredictions']
+        emotions_dict = prediction_details['emotion']
+        # 2. Find the name of the dominant emotion
+        dominant_name = max(emotions_dict, key=emotions_dict.get)
+        # 3. Construct the output in your requested dictionary format
+        result = {
+            'anger': emotions_dict['anger'],
+            'disgust': emotions_dict['disgust'],
+            'fear': emotions_dict['fear'],
+            'joy': emotions_dict['joy'],
+            'sadness': emotions_dict['sadness'],
+            'dominant_emotion': dominant_name
+        }
+        # Print the final result
+        print(result)
+
+    
+
+
+
         # label = formatted_response['documentSentiment']['label']
         # score = formatted_response['documentSentiment']['score']
     elif response.status_code == 500:
@@ -22,4 +43,4 @@ def emotion_detector(text_to_analyze):
     # if response.status_code == 200:
 
     # from emotion_detection import emotion_detector
-    # emotion_detector("Hello how are you")
+    # emotion_detector("I love this new technology.")
